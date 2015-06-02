df1a9409529a5d3aab8176bbf50f9493b4489dc8
yun dai <yun.dai@dailydealsbox.com>
2015-06-02 03:48:34 +0000

    PM-21 pay monthly proudct service interface and implementation class

---
+++ src/main/java/com/dailydealsbox/database/service/PayMonthlyProductsServiceImpl.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.dailydealsbox.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dailydealsbox.database.dao.PayMonthlyProductsDao;
+import com.dailydealsbox.database.model.PayMonthlyProduct;
+
+/**
+ * @author x_ye
+ */
+@Service("timo_test_service")
+@Transactional
+public class PayMonthlyProductsServiceImpl implements PayMonthlyProductsService {
+
+  @Autowired
+  private PayMonthlyProductsDao dao;
+
+  /*
+   * (non-Javadoc)
+   * @see
+   * com.dailydealsbox.database.service.PayMonthlyProductsService#save(com.dailydealsbox.database.model.PayMonthlyProduct
+   * )
+   */
+  @Override
+  public void save(PayMonthlyProduct paymonthlyproduct) {
+    dao.save(paymonthlyproduct);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see com.dailydealsbox.database.service.PayMonthlyProductsService#findAll()
+   */
+  @Override
+  public List<PayMonthlyProduct> findAll() {
+    return dao.findAll();
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see com.dailydealsbox.database.service.TestService#get(int)
+   */
+  @Override
+  public PayMonthlyProduct get(int id) {
+    return dao.get(id);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see
+   * com.dailydealsbox.database.service.PayMonthlyProductsService#delete(com.dailydealsbox.database.model.
+   * PayMonthlyProduct)
+   */
+  @Override
+  public void delete(PayMonthlyProduct paymonthlyproduct) {
+    dao.delete(paymonthlyproduct);
+  }
+
+}
+
