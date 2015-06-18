7e9838d97a7613ad9b8331cf358bb115e515670f
unknown <y_dai@MTL1-W-PC-02212.mgcorp.co>
2015-06-18 16:29:05 -0400

    product form revised

--- src/main/webapp/admin_portal/admin-controllers.js
+++ src/main/webapp/admin_portal/admin-controllers.js
@@ -24,7 +24,9 @@ angular.module('adminApp.controllers', [ 'angular-md5' ])
         'PMCtrl',
         [ '$scope', '$location', 'ProductService',
                 function($scope, $location, ProductService) {
-                    ProductService.home(function(response) {
+        	
+                    
+        	        ProductService.home(function(response) {
                         if (response.status == 'SUCCESS') {
                             $scope.items = response.data;
                         }
@@ -39,6 +41,22 @@ angular.module('adminApp.controllers', [ 'angular-md5' ])
         [ '$scope', '$location', '$routeParams', 'ProductService',
                 function($scope, $location, $routeParams, ProductService, MenuService) {
                     var id = $routeParams.id;
+        	        
+                    $scope.status =
+        	        	[
+        	        	 { id: 1, name: "ACTIVE" },
+        	        	 { id: 2, name: "INACTIVE" },
+        	        	 { id: 3, name: "AVAILABLE" }
+        	            ];                     
+        	        
+                    $scope.stores =
+        	        	[
+        	        	 { id: 1, type: "Online", name: "walmart.ca" },
+        	        	 { id: 2, type: "Online", name: "ebay.ca" },
+        	        	 { id: 3, type: "Online", name: "costco.ca" },
+        	        	 { id: 4, type: "Local", name: "Brick" }
+        	            ];                      
+                    
                     ProductService.get(id, function(response) {
                         if (response.status == 'SUCCESS') {
                             $scope.product = response.data;
