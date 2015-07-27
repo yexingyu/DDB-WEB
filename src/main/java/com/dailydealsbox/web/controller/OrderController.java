9323ceb8da01f555f45cf5d4e80da49d3eeb2692
Xingyu Ye <yexingyu@gmail.com>
2015-07-26 23:02:00 -0400

    modified order controller

--- src/main/java/com/dailydealsbox/web/controller/OrderController.java
+++ src/main/java/com/dailydealsbox/web/controller/OrderController.java
@@ -49,36 +49,35 @@ public class OrderController {
    *
    * @param deleted
    * @param pageable
-   *
    * @return
    */
   @RequestMapping(method = RequestMethod.GET)
   @ApiOperation(value = "list orders",
-      response = GenericResponseData.class,
-      responseContainer = "Map",
-      produces = "application/json",
-      notes = "List pageable orders.")
+    response = GenericResponseData.class,
+    responseContainer = "Map",
+    produces = "application/json",
+    notes = "List pageable orders.")
   @ApiImplicitParams({@ApiImplicitParam(name = "page", value = "page number", required = false, defaultValue = "0", dataType = "int", paramType = "query"),
-      @ApiImplicitParam(name = "size", value = "page size", required = false, defaultValue = "20", dataType = "int", paramType = "query"), @ApiImplicitParam(
-      name = "sort", value = "sorting. (eg. &sort=createdAt,desc)", required = false, defaultValue = "", dataType = "String", paramType = "query")})
-  public GenericResponseData list (
-      @ApiParam(value = "filter: is deleted", required = false, defaultValue = "false") @RequestParam(value = "deleted",
-          required = false,
-          defaultValue = "false") boolean deleted,
-      @ApiIgnore @CookieValue(value = "token", required = false) String tokenString, @ApiIgnore Pageable pageable) {
+    @ApiImplicitParam(name = "size", value = "page size", required = false, defaultValue = "20", dataType = "int", paramType = "query"), @ApiImplicitParam(
+    name = "sort", value = "sorting. (eg. &sort=createdAt,desc)", required = false, defaultValue = "", dataType = "String", paramType = "query")})
+  public GenericResponseData list(
+    @ApiParam(value = "filter: is deleted", required = false, defaultValue = "false") @RequestParam(value = "deleted",
+      required = false,
+      defaultValue = "false") boolean deleted,
+    @ApiIgnore @CookieValue(value = "token", required = false) String tokenString, @ApiIgnore Pageable pageable) {
 
-    AuthorizationToken token = this.authorizationService.verify (tokenString);
+    AuthorizationToken token = this.authorizationService.verify(tokenString);
     if (token == null) {
-      return GenericResponseData.newInstance (RESPONSE_STATUS.NEED_LOGIN, "");
-    } else if (token.getRole () == MEMBER_ROLE.ADMIN) {
-      Page<Order> orders = this.orderService.list (deleted, pageable);
-      if (orders == null || orders.getNumberOfElements () == 0) {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.EMPTY_RESULT, "");
+      return GenericResponseData.newInstance(RESPONSE_STATUS.NEED_LOGIN, "");
+    } else if (token.getRole() == MEMBER_ROLE.ADMIN) {
+      Page<Order> orders = this.orderService.list(deleted, pageable);
+      if (orders == null || orders.getNumberOfElements() == 0) {
+        return GenericResponseData.newInstance(RESPONSE_STATUS.EMPTY_RESULT, "");
       } else {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.SUCCESS, orders);
+        return GenericResponseData.newInstance(RESPONSE_STATUS.SUCCESS, orders);
       }
     } else {
-      return GenericResponseData.newInstance (RESPONSE_STATUS.NO_PERMISSION, "");
+      return GenericResponseData.newInstance(RESPONSE_STATUS.NO_PERMISSION, "");
     }
   }
 
@@ -86,34 +85,33 @@ public class OrderController {
    * retrieve
    *
    * @param id
-   *
    * @return
    */
   @RequestMapping(value = "id/{id}", method = RequestMethod.GET)
   @ApiOperation(value = "retrieve order details",
-      response = GenericResponseData.class,
-      responseContainer = "Map",
-      produces = "application/json",
-      notes = "Retrieve order details.")
-  public GenericResponseData retrieve (@ApiParam(value = "order id", required = true) @PathVariable("id") int id,
-                                       @ApiIgnore @CookieValue(value = "token", required = false) String tokenString) {
+    response = GenericResponseData.class,
+    responseContainer = "Map",
+    produces = "application/json",
+    notes = "Retrieve order details.")
+  public GenericResponseData retrieve(@ApiParam(value = "order id", required = true) @PathVariable("id") int id,
+                                      @ApiIgnore @CookieValue(value = "token", required = false) String tokenString) {
 
-    AuthorizationToken token = this.authorizationService.verify (tokenString);
+    AuthorizationToken token = this.authorizationService.verify(tokenString);
     if (token == null) {
-      return GenericResponseData.newInstance (RESPONSE_STATUS.NEED_LOGIN, "");
-    } else if (token.getRole () == MEMBER_ROLE.ADMIN) {
-      Order order = this.orderService.get (id);
+      return GenericResponseData.newInstance(RESPONSE_STATUS.NEED_LOGIN, "");
+    } else if (token.getRole() == MEMBER_ROLE.ADMIN) {
+      Order order = this.orderService.get(id);
       if (order == null) {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.EMPTY_RESULT, "");
+        return GenericResponseData.newInstance(RESPONSE_STATUS.EMPTY_RESULT, "");
       } else {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.SUCCESS, order);
+        return GenericResponseData.newInstance(RESPONSE_STATUS.SUCCESS, order);
       }
     } else {
-      Order order = this.orderService.get (id);
-      if (order == null || order.getMemberId () != token.getMemberId ()) {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.NO_PERMISSION, "");
+      Order order = this.orderService.get(id);
+      if (order == null || order.getMemberId() != token.getMemberId()) {
+        return GenericResponseData.newInstance(RESPONSE_STATUS.NO_PERMISSION, "");
       } else {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.SUCCESS, order);
+        return GenericResponseData.newInstance(RESPONSE_STATUS.SUCCESS, order);
       }
     }
   }
@@ -123,55 +121,26 @@ public class OrderController {
    *
    * @param tokenString
    * @param order
-   *
    * @return
    */
   @RequestMapping(method = {RequestMethod.POST})
   @ApiOperation(value = "insert order",
-      response = GenericResponseData.class,
-      responseContainer = "Map",
-      produces = "application/json",
-      notes = "Insert a new order.")
-  public GenericResponseData insert (@ApiIgnore @CookieValue(value = "token", required = false) String tokenString,
-                                     @ApiParam(value = "order object", required = true) @RequestBody Order order) {
-    AuthorizationToken token = this.authorizationService.verify (tokenString);
+    response = GenericResponseData.class,
+    responseContainer = "Map",
+    produces = "application/json",
+    notes = "Insert a new order.")
+  public GenericResponseData insert(@ApiIgnore @CookieValue(value = "token", required = false) String tokenString,
+                                    @ApiParam(value = "order object", required = true) @RequestBody Order order) {
+    AuthorizationToken token = this.authorizationService.verify(tokenString);
     if (token == null) {
-      return GenericResponseData.newInstance (RESPONSE_STATUS.NEED_LOGIN, "");
+      return GenericResponseData.newInstance(RESPONSE_STATUS.NEED_LOGIN, "");
     } else {
-      order.setStatus (BaseEnum.ORDER_STATUS.NEW);
-      Order orderFromDb = this.orderService.insert (order);
-      return GenericResponseData.newInstance (RESPONSE_STATUS.SUCCESS, orderFromDb);
+      order.setStatus(BaseEnum.ORDER_STATUS.NEW);
+      Order orderFromDb = this.orderService.insert(order);
+      return GenericResponseData.newInstance(RESPONSE_STATUS.SUCCESS, orderFromDb);
     }
   }
 
-  /**
-   * confirm
-   *
-   * @param orderId
-   * @param tokenString
-   *
-   * @return
-   */
-  @RequestMapping(value = "id/{id}/confirm", method = {RequestMethod.PUT})
-  @ApiOperation(value = "confirm order",
-      response = GenericResponseData.class,
-      responseContainer = "Map",
-      produces = "application/json",
-      notes = "Customer confirm order.")
-  public GenericResponseData confirm (@ApiParam(value = "order id", required = true) @PathVariable("id") int orderId,
-                                      @ApiIgnore @CookieValue(value = "token", required = false) String tokenString) {
-    AuthorizationToken token = this.authorizationService.verify (tokenString);
-    if (token == null) {
-      return GenericResponseData.newInstance (RESPONSE_STATUS.NEED_LOGIN, "");
-    } else {
-      Order order = this.orderService.get (orderId);
-      if (order == null || order.getMemberId () != token.getMemberId ()) {
-        return GenericResponseData.newInstance (RESPONSE_STATUS.NO_PERMISSION, "");
-      }
-      order = this.orderService.modifiyStatus (orderId, BaseEnum.ORDER_STATUS.CONFIRMED);
-      return GenericResponseData.newInstance (RESPONSE_STATUS.SUCCESS, order);
-    }
-  }
 
   /**
    * switchStatus
@@ -181,19 +150,21 @@ public class OrderController {
    * @param tokenString
    * @return
    */
-  @RequestMapping(value = "id/{id}/switch_status", method = { RequestMethod.PUT })
+  @RequestMapping(value = "id/{id}/switch_status", method = {RequestMethod.PUT})
   @ApiOperation(value = "switch status",
     response = GenericResponseData.class,
     responseContainer = "Map",
     produces = "application/json",
     notes = "Switch status of order.")
   public GenericResponseData switchStatus(@ApiParam(value = "order id", required = true) @PathVariable("id") int orderId,
-      @ApiParam(value = "order object", required = true) @RequestBody String status,
-      @ApiIgnore @CookieValue(value = "token", required = false) String tokenString) {
+                                          @ApiParam(value = "order object", required = true) @RequestBody String status,
+                                          @ApiIgnore @CookieValue(value = "token", required = false) String tokenString) {
     AuthorizationToken token = this.authorizationService.verify(tokenString);
     if (token == null) {
       return GenericResponseData.newInstance(RESPONSE_STATUS.NEED_LOGIN, "");
-    } else if (token.getRole() == MEMBER_ROLE.ADMIN) { return GenericResponseData.newInstance(RESPONSE_STATUS.NO_PERMISSION, ""); }
+    } else if (token.getRole() == MEMBER_ROLE.ADMIN) {
+      return GenericResponseData.newInstance(RESPONSE_STATUS.NO_PERMISSION, "");
+    }
 
     BaseEnum.ORDER_STATUS orderStatus = BaseEnum.ORDER_STATUS.valueOf(status);
     this.orderService.modifiyStatus(orderId, orderStatus);
