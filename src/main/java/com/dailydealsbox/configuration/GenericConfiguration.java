0d4f79f55ca973f1b349ddbd90e18134bd7274b3
Yun Dai <Yun.Dai@mindgeek.com>
2015-08-24 15:32:01 -0400

    update exchange rate

--- src/main/java/com/dailydealsbox/configuration/GenericConfiguration.java
+++ src/main/java/com/dailydealsbox/configuration/GenericConfiguration.java
@@ -10,5 +10,5 @@ public class GenericConfiguration {
   public static double ARP               = 0.24;
   public static int    PAYMENTS          = 12;
   public static double DOWNPAYMENT_RATE  = 0.382;
-  public static double EXCHANGE_RATE_USD = 1.30;
+  public static double EXCHANGE_RATE_USD = 1.3588;
 }
